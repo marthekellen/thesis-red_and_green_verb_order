328e1bfe660e5132c285d3debc1e0bcb38f40ff5
marthekellen <150336504+marthekellen@users.noreply.github.com>
2024-06-06 17:56:29 +0200

    Update 4) predicting.py

--- 4) predicting.py
+++ 4) predicting.py
@@ -114,7 +114,7 @@ def compute_metrics(pred):
 training_args = TrainingArguments(
     output_dir='./results',          # output directory
     num_train_epochs=2,              # total number of training epochs
-    per_device_train_batch_size=32,  # batch size per device during training
+    per_device_train_batch_size=64,  # batch size per device during training
     per_device_eval_batch_size=64,   # batch size for evaluation
     warmup_steps=100,                # number of warmup steps for learning rate scheduler
     weight_decay=0.01,               # strength of weight decay
